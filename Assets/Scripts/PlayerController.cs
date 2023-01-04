6fe74f82348482fd93bb03d83cae1e0029e37e66
Mourat <mourat@boutry.me>
2023-01-04 14:45:54 +0100

    Created prefabs for projectile and animals. Added script for movement des objets.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,26 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed;
-    /* Use a fixed time step: If your game is running at a fixed frame rate,
-    you can use a fixed time step to update the object's position,
-    rather than using Time.deltaTime. This can help improve the consistency
-    of the object's movement, as well as the performance of your code. */
+    /*
+     * Fixed time step: If game is running at a fixed frame rate,
+     * you can use a fixed time step to update the object's position,
+     * rather than using Time.deltaTime. This can help improve the consistency
+     * of the object's movement, as well as the performance of your code.
+     */
     private const float TimeStep = 1.0f / 60.0f;
     private Vector3 _right;
     [SerializeField] private float xRange;
+    [SerializeField] private GameObject projectilePrefab;
+    
 
     private void Reset()
     {
         speed = 10.0f;
         xRange = 15f;
+        projectilePrefab = GameObject.Find("P_Food_Pizza");
     }
 
     private void Awake()
@@ -30,12 +33,18 @@ public class PlayerController : MonoBehaviour
         var pos = transform.position;
         var horizontalInput = Input.GetAxis("Horizontal");
         
-        /* Mathf.Clamp function to constrain the x coordinate of the object's position
-        to the range [-24, 24]. This is more concise than using an if statement.*/ 
+        /*
+         * Mathf.Clamp function to constrain the x coordinate of the object's position
+         * to the range [-24, 24]. This is more concise than using an if statement.
+         */ 
         pos.x = Mathf.Clamp(pos.x, -xRange, xRange);
         
         transform.position = pos;
-        
         transform.Translate(_right * horizontalInput * TimeStep * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, pos, projectilePrefab.transform.rotation);
+        }
     }
 }
